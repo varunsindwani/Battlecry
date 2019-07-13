b0f3dd9bc90e277e9f92d61cb1cf153308415b46
Varun Sindwani <varunsindwani87@gmail.com>
2019-07-13 00:38:30 -0400

    Image system implemented

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image'
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "images/war.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1300) {
-            ...GatsbyImageSharpFluid
+const Image = (props) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                sizes(maxWidth: 600) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
           }
         }
       }
-    }
-  `);
+    `}
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
-};
+    render={(data) => {
+      const image = data.images.edges.find(n => {
+        return n.node.relativePath.includes(props.filename);
+      });
+      if (!image) { return null; }
+      
+      const imageSizes = image.node.childImageSharp.sizes;
+      return (
+        <Img
+          alt={props.alt}
+          sizes={imageSizes}
+        />
+      );
+    }}
+  />
+)
 
 export default Image;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
-
-import Layout from '../components/layout';
-import Image from '../components/image';
-import SEO from '../components/seo';
+import HomePage from './home'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="block mx-auto w-4/5 text-center p-2">
-      <Image />
-    </div>
-    <Link to="/page-2/" className="text-center font-bold underline">Click to 404</Link>
-  </Layout>
+  <HomePage />
 );
 
 export default IndexPage;
